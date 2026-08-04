bed96a88a50f599fd826b7eee2a5d704e9992aa3
Anick03 <anickdahal03@gmail.com>
2026-08-04 22:00:11 +0545

    main

---
+++ Javascript/day_12/script.js
@@ -0,0 +1,39 @@
+// let age = 18
+// let nationality = "nepali"
+// if (age >= 18 && nationality == "nepali") {
+//     console.log("You are eligible to vote.");
+// }
+// else if (age >= 18 && nationality != "nepali") {
+//     console.log("You are not eligible to vote because you are not the citizen of this country.");
+// }
+// else {
+//     console.log("You are not eligible to vote.");
+// }
+
+let x = 20
+let y = 10
+let s = x + y
+console.log(s)
+console.log(x * y)
+let ram = "girl"
+if (ram == "boy") {
+    console.log("Ram is boy");
+}
+else {
+    console.log("Ram is a girl");
+}
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// let i = 0
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+let i = 10
+do {
+    console.log(i)
+    i--
+} while (i > 0)
